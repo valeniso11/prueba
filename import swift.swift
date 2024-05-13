c6171c3e9da302619807075a3e9ad171728d4664
valeniso11 <visolabella@frba.utn.edu.ar>
2024-05-13 18:38:25 -0300

    Primeras pruebas en swift

---
+++ import swift.swift
@@ -0,0 +1,15 @@
+import Swift
+
+print (" holaa mundo")
+
+var ElPrimerString = "Hello World"
+
+print(ElPrimerString)
+
+var PrimerInt = 20
+
+print(PrimerInt)
+
+PrimerInt += 2
+
+print(PrimerInt)
