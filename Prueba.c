077d3dfd54bc999db1133905903b371dc744f45c
valentin isolabella <visolabella@frba.utn.edu.ar>
2024-05-31 19:58:34 -0300

    Prueba haskell y c para compu de cata, falta compilador c

---
+++ Prueba.c
@@ -0,0 +1,9 @@
+
+int main(int argc, char const *argv[])
+{
+    int num; 
+    printf("ingrese un numero: "); 
+    scanf("%d", &num);
+    printf("\n tu numero es: %d \n ", num );
+    return 0;
+}
